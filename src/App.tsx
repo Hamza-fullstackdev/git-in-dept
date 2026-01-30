89ac23a64782d0c28fc731e501e0bdd5b97805be
hamza-fullstackdev <hamzafullstackdev1@gmail.com>
2026-01-30 09:36:41 +0500

    Design ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 function App() {
   return (
     <main className='py-10'>
-      <section className='mx-auto w-full md:w-5xl border border-gray-100'>
+      <section className='mx-auto w-full md:w-5xl border border-gray-100 rounded-md'>
         <div className='p-6'>
-          <h1 className='text-center text-2xl font-bold'>
-            Lets understand git in more depth
-          </h1>
+          <div className='flex flex-col gap-1 text-center'>
+            <h1 className='text-3xl font-bold'>
+              Lets understand git in more depth
+            </h1>
+            <span className='text-gray-400'>
+              Lets start by building a counter in a new branch
+            </span>
+          </div>
+          <div className='mx-auto w-fit mt-6 flex items-center gap-4'>
+            <button className='bg-red-500 text-sm text-white px-3 py-2 rounded-md'>
+              Decrement
+            </button>
+            <span>1</span>
+            <button className='bg-blue-500 text-sm text-white px-3 py-2 rounded-md'>
+              Increment
+            </button>
+          </div>
         </div>
       </section>
     </main>
