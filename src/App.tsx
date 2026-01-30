54ca55cc9d0d804b05d006092934db1bb470296b
hamza-fullstackdev <hamzafullstackdev1@gmail.com>
2026-01-30 09:22:24 +0500

    Setup project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <main className='py-10'>
+      <section className='mx-auto w-full md:w-5xl border border-gray-100'>
+        <div className='p-6'>
+          <h1 className='text-center text-2xl font-bold'>
+            Lets understand git in more depth
+          </h1>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
